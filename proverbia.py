a4fd701d9e2b04acb26130b4f6884728fdb6f1d6
Jesus G.H <jesuspuronervio@gmail.com>
2022-06-19 20:42:54 +0200

    Creada como función y return como lista

--- proverbia.py
+++ proverbia.py
@@ -1,18 +1,23 @@
 import requests
 
 url_base = 'https://proverbia.net/frase-del-dia'
-text = requests.get(url_base) #Respuesta
-rawText = text.content.decode() #Contenido en bruto
 
-posFrase = rawText.find('class="bsquote"')
-iniFrase = rawText.find('<p>', posFrase)
-finFrase = rawText.find('</p', posFrase)
-frase = rawText[iniFrase+3:finFrase]
+def getFrase():
+    text = requests.get(url_base) #Respuesta
+    rawText = text.content.decode() #Contenido en bruto
 
-posAutor = rawText.find('<a title=', finFrase)
-iniAutor = rawText.find('>', posAutor)
-finAutor = rawText.find('</a>', iniAutor)
-autor = rawText[iniAutor+2:finAutor]
+    posFrase = rawText.find('class="bsquote"')
+    iniFrase = rawText.find('<p>', posFrase)
+    finFrase = rawText.find('</p', posFrase)
+    frase = rawText[iniFrase+3:finFrase]
 
-print(frase)
-print(autor)
+    posAutor = rawText.find('<a title=', finFrase)
+    iniAutor = rawText.find('>', posAutor)
+    finAutor = rawText.find('</a>', iniAutor)
+    autor = rawText[iniAutor+2:finAutor]
+
+    return frase, autor
+
+proverbia = getFrase()
+print(proverbia[0])
+print(proverbia[1])
